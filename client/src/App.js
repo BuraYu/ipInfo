83b8727eba8b8016dc64c3d1fda1f7cce96d8422
BuraYu <burak.yueksel@hotmail.com>
2024-07-22 22:23:36 +0200

    add Typewriter effect, Cursor. Changed the layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,134 +1,137 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 function App() {
-  const [link, setLink] = useState("google.de");
-  const [ipInput, setIpInput] = useState(false);
+  const [status, setStatus] = useState("");
+  const [timezone, setTimezone] = useState("");
+  const [region, setRegion] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [continent, setContinent] = useState("");
+  const [currency, setCurrency] = useState("");
+  const [geoLocation, setGeoLocation] = useState("");
   const [ip, setIp] = useState("");
-  const [browser, setBrowser] = useState("");
-  const [os, setOs] = useState("");
-  const [device, setDevice] = useState("");
-  const [language, setLanguage] = useState("");
-  const [referrer, setReferrer] = useState("");
 
-  const createLink = () => {
-    setLink("https://ip-info-client.vercel.app/" + randomChar(8));
-  };
+  const [text] = useTypewriter({
+    words: ["Hello", "timezone", "region", "mobile"],
+    loop: false,
+    onLoopDone: () => console.log(`loop completed after 3 runs.`),
+    typeSpeed: 300,
+    delaySpeed: 1000,
+  });
 
-  const randomChar = (length) => {
-    let result = "";
-    const characters =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
-    }
-    return result;
-  };
-
+  //ip-api 45 uses per minute, throttle after
   const apiCall = async () => {
-    try {
-      const response = await axios.get(
-        "https://ip-info-server.vercel.app/api/get-info"
-      );
-      const data = response.data;
-      setIp(data.ip);
-      setBrowser(`${data.browser.name} ${data.browser.version}`);
-      setOs(OsConverter(data.os.name, data.os.version));
-      setDevice(data.device.type || "Unknown");
-      setLanguage(data.language);
-      setReferrer(data.referrer);
-      setIpInput(true);
-    } catch (error) {
-      console.error("Error fetching the client info:", error);
-    }
-  };
-
-  const apiCall2 = async () => {
     try {
       const response = await axios.get(
         "http://ip-api.com/json/2003:D0:973B:4A58:EC2A:9304:1C29:FEED?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query"
       );
-      const data2 = response.data;
-      console.log(data2);
+      const data = response.data;
+      setStatus(data.status);
+      setTimezone(data.timezone);
+      setRegion(data.region);
+      setMobile(data.mobile);
+      setCity(data.city);
+      setCountry(data.country);
+      setCurrency(data.currency);
+      setContinent(data.continent);
+      setGeoLocation(`Latitude: ${data.lat} Longtitude: ${data.lon}`);
+      setIp(data.geoLocation);
+      setIp(data.query);
     } catch (error) {
       console.error("Error fetching the client info:", error);
     }
   };
 
-  const OsConverter = (nameOs, version) => {
-    console.log(nameOs);
-    console.log(version);
-    if (version === "NT 10.0") {
-      return "Windows 10";
-    } else if (version === "NT 11.0") {
-      return "Windows 11";
-    } else return nameOs + version;
-  };
+  useEffect(() => {
+    apiCall();
+    console.log("called once!");
+  }, []);
 
   return (
-    <div className="container">
-      <h1>IP Grabber</h1>
-      <div className="link--container">
-        <span>Link</span>
-        <input type="text" name="telnum" value={link} readOnly="readonly" />
-        <button onClick={createLink}>Create link</button>
-      </div>
-      <div>
-        <span>IP address</span>
-        <input
-          className={ipInput ? "valid" : "error"}
-          type="text"
-          name="telnum"
-          value={ip}
-          readOnly="readonly"
-        />
-        <span>Browser</span>
-        <input
-          className={ipInput ? "valid" : "error"}
-          type="text"
-          name="telnum"
-          value={browser}
-          readOnly="readonly"
-        />
-        <span>OS</span>
-        <input
-          className={ipInput ? "valid" : "error"}
-          type="text"
-          name="telnum"
-          value={os}
-          readOnly="readonly"
-        />
-        <span>Device</span>
-        <input
-          className={ipInput ? "valid" : "error"}
-          type="text"
-          name="telnum"
-          value={device}
-          readOnly="readonly"
-        />
-        <span>Language</span>
-        <input
-          className={ipInput ? "valid" : "error"}
-          type="text"
-          name="telnum"
-          value={language}
-          readOnly="readonly"
-        />
-        <span>Referrer</span>
-        <input
-          className={ipInput ? "valid" : "error"}
-          type="text"
-          name="telnum"
-          value={referrer}
-          readOnly="readonly"
-        />
-        <button onClick={apiCall}>Get Client Info</button>
-        <button onClick={apiCall2}>Get Client Info</button>
-      </div>
+    // <div className="container">
+    //   <div>
+    //     <input
+    //       className={"valid"}
+    //       type="text"
+    //       name="telnum"
+    //       value={status}
+    //       readOnly="readonly"
+    //     />
+    //     <span>Timezone</span>
+    //     <input
+    //       className={"valid"}
+    //       type="text"
+    //       name="telnum"
+    //       value={timezone}
+    //       readOnly="readonly"
+    //     />
+    //     <span>City</span>
+    //     <input
+    //       className={"valid"}
+    //       type="text"
+    //       name="telnum"
+    //       value={city}
+    //       readOnly="readonly"
+    //     />
+    //     <span>Mobile Device</span>
+    //     <input
+    //       className={"valid"}
+    //       type="text"
+    //       name="telnum"
+    //       value={mobile}
+    //       readOnly="readonly"
+    //     />
+    //     <span>Country</span>
+    //     <input
+    //       className={"valid"}
+    //       type="text"
+    //       name="telnum"
+    //       value={country}
+    //       readOnly="readonly"
+    //     />
+    //     <span>Paying in</span>
+    //     <input
+    //       className={"valid"}
+    //       type="text"
+    //       name="telnum"
+    //       value={currency}
+    //       readOnly="readonly"
+    //     />
+    //     <span>Geo Location</span>
+    //     <input
+    //       className={"valid"}
+    //       type="text"
+    //       name="telnum"
+    //       value={geoLocation}
+    //       readOnly="readonly"
+    //     />
+    //     <span>Continent</span>
+    //     <input
+    //       className={"valid"}
+    //       type="text"
+    //       name="telnum"
+    //       value={continent}
+    //       readOnly="readonly"
+    //     />
+    //     <span>Ip Adress</span>
+    //     <input
+    //       className={"valid"}
+    //       type="text"
+    //       name="telnum"
+    //       value={ip}
+    //       readOnly="readonly"
+    //     />
+    //     <button onClick={apiCall2}>Get Client Info</button>
+    //   </div>
+
+    // </div>
+    <div className="test">
+      <span>{text}</span>
+      <Cursor cursorColor="#20C20E" />
     </div>
   );
 }
