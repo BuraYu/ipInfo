ba0ddce2f89ef9ab16183ceedec4f646026681f3
BuraYu <burak.yueksel@hotmail.com>
2024-07-23 20:17:03 +0200

    moved api data in useEffect hook

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
     geoLocation: "",
     ip: "",
   });
-
+  const url =
+    "http://ip-api.com/json/2003:D0:973B:4A58:EC2A:9304:1C29:FEED?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query";
+  const [apiCallData, setApiCallData] = useState([]);
   //ip-api 45 uses per minute, throttle after
   const apiCall = async () => {
     try {
-      const { data } = await axios.get(
-        "http://ip-api.com/json/2003:D0:973B:4A58:EC2A:9304:1C29:FEED?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query"
-      );
+      const { data } = await axios.get(url);
       const { status, mobile, city, country, currency, lat, lon, query } = data;
       setClientData({
         status,
@@ -38,37 +38,24 @@ function App() {
 
   useEffect(() => {
     apiCall();
-    console.log("called once!");
+    setApiCallData([
+      "Hello",
+      "you shouldn't have clicked on the link...",
+      `This was a ${clientData.status}`,
+      clientData.mobile
+        ? "So you use your cellphone"
+        : "Not using your cellphone, I see",
+      `${clientData.country} is a nice country.`,
+      `You are from ${clientData.city}, right?`,
+      `Maybe you can send me some ${clientData.currency}?`,
+      `Since I have your exact location ${clientData.geoLocation}`,
+      `and your IP address ${clientData.ip}`,
+      "clicking the link was a big mistake....",
+    ]);
   }, []);
 
-  const apiCallData = [
-    "Hello",
-    "you shouldn't have clicked on the link...",
-    `This was a ${clientData.status}`,
-    clientData.mobile
-      ? "So you use your cellphone"
-      : "Not using your cellphone, I see",
-    `${clientData.country} is a nice country.`,
-    `You are from ${clientData.city}, right?`,
-    `Maybe you can send me some ${clientData.currency}?`,
-    `Since I have your exact location ${clientData.geoLocation}`,
-    `and your IP address ${clientData.ip}`,
-    "clicking the link was a big mistake....",
-  ];
-
   const [text] = useTypewriter({
-    words: [
-      apiCallData[0],
-      apiCallData[1],
-      apiCallData[2],
-      apiCallData[3],
-      apiCallData[4],
-      apiCallData[5],
-      apiCallData[6],
-      apiCallData[7],
-      apiCallData[8],
-      apiCallData[9],
-    ],
+    words: apiCallData,
     loop: false,
     typeSpeed: 100,
     delaySpeed: 1000,
