539f1297a3b5c1836647aaee3bb8a3a4546cbaae
BuraYu <burak.yueksel@hotmail.com>
2024-07-24 23:06:18 +0200

    add leaflet to code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 function App() {
   const [clientData, setClientData] = useState({
@@ -12,6 +15,8 @@ function App() {
     currency: "",
     geoLocation: "",
     ip: "",
+    lon: 0,
+    lat: 0,
   });
 
   const url =
@@ -30,6 +35,8 @@ function App() {
         country,
         currency,
         geoLocation: `Latitude ${lat} Longitude: ${lon}`,
+        lon,
+        lat,
         ip: query,
       });
       console.log(data);
@@ -56,12 +63,32 @@ function App() {
     ]);
   }, []);
 
+  const position = [clientData.lat, clientData.lon];
+
   const [text] = useTypewriter({
     words: apiCallData,
     loop: false,
     typeSpeed: 100,
     delaySpeed: 1000,
   });
+
+  delete L.Icon.Default.prototype._getIconUrl;
+
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+    iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+  });
+
+  const RecenterMap = ({ lat, lon }) => {
+    const map = useMap();
+    useEffect(() => {
+      if (lat && lon) {
+        map.setView([lat, lon], 13);
+      }
+    }, [lat, lon]);
+    return null;
+  };
 
   return (
     <div className="test">
@@ -73,9 +100,17 @@ function App() {
         <span>{clientData.country}</span>
         <span>{clientData.currency}</span>
         <span>{clientData.ip}</span>
+        <MapContainer
+          center={position}
+          zoom={2}
+          style={{ height: "500px", width: "500px" }}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={position} />
+          <RecenterMap lat={clientData.lat} lon={clientData.lon} />
+        </MapContainer>
       </div>
     </div>
   );
 }
-
 export default App;
